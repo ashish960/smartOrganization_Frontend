9b8d90e4cb9512f35e165085805c80a2231ac6e8
Ashish Painuly <ashishpainuly@smartdatainc.net>
2026-05-08 17:56:22 +0530

    initial update

---
+++ frontend/src/store/authSlice.ts
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface AuthState {
+  user: null | {
+    id: string;
+    name: string;
+    email: string;
+  };
+  token: string | null;
+}
+
+const initialState: AuthState = {
+  user: null,
+  token: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    loginSuccess: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+
+    logout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+  },
+});
+
+export const { loginSuccess, logout } = authSlice.actions;
+export default authSlice.reducer;
